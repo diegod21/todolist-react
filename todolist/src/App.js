26cef474bf2be6c7a0002124f2f802b75d1ce21e
diegod21 <108184501+diegod21@users.noreply.github.com>
2023-08-25 14:27:24 -0300

    starting tolist-react

---
+++ todolist/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <h1>Todo</h1>
+
+      <form>
+        <input type="text"></input>
+        <button>Add</button>
+      </form>
+
+      <ul>
+        <li>
+          item
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
