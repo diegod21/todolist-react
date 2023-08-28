101714235e17544be4984c7ee9070d0559fa3eb3
diegod21 <108184501+diegod21@users.noreply.github.com>
2023-08-28 12:22:09 -0300

    .

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,20 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  
+  const [text , setText] = useState('') 
+  const [items, setItems] = useState([])
+    function changeHandle(event){
+    let t = event.target.value
+    setText(t)
+  }
+  function addItem(event){
+    event.preventDefault()
+    if (text) {
+      setItems([...items, text])
+      setText("")
+    }
+
+  }
   return (
     <div className="">
       <h1>Todo</h1>
 
       <form>
-        <input type="text"></input>
-        <button>Add</button>
+        <input onChange={changeHandle} type="text" value={text}></input>
+        <button onClick={addItem}>Add</button>
       </form>
 
       <ul>
-        <li>
-          item
-        </li>
+      {items.map(item=> <li>{item}</li>)}
       </ul>
     </div>
   );
